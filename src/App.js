a21f65c77ed264b52a4269198f7c988781e7936c
najmuleimon <najmul.eimon@gmail.com>
2022-04-03 21:21:59 +0600

    components and routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Review from './components/Review/Review';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
-      
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/review" element={<Review/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/blogs" element={<Blogs/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <p>this is 404 not found</p>
+        </div>
+    );
+};
+
+export default NotFound;
