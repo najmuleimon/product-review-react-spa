eddf74671f53c35952c80f2ef0a7f45f928c39ef
najmuleimon <najmul.eimon@gmail.com>
2022-04-03 22:12:12 +0600

    navigation and home banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Home.css';
+import watch from '../../images/watch.png';
 
 const Home = () => {
     return (
-        <div>
-            <p>this is home</p>
+        <div className='banner'>
+            <div className="container">
+                <div className="text-part">
+                    <h1>Welcome to <span>Rewatch</span></h1>
+                    <h2>Watch Review</h2>
+                    <p>Introducing Rewatch Wild Approach To Watchmaking Is On Full Display At Watches & Wonders. Hundreds of fresh watches have arrived to Rewatch pre-owned. Photo ReportMoonSwatch Madness!</p>
+                </div>
+                <div className="image-part">
+                    <img src={watch} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
-            <p>this is navigation</p>
-        </div>
+        <nav className='navigation'>
+            <div className="container">
+                <ul className='nav-links'>
+                    <li>
+                        <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/review'>Review</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/dashboard'>Dashboard</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => (isActive ? "active-link" : "link")} to='/blogs'>Blogs</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
