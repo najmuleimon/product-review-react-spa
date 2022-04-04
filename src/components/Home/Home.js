036f7e075ee967a414912d9d0d8e283dddd6e227
najmuleimon <najmul.eimon@gmail.com>
2022-04-04 22:56:02 +0600

    folder moved

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Home.css';
 import watch from '../../images/watch.png';
-import useReview from '../hooks/useReview';
+import useReview from '../../hooks/useReview';
 import Review from '../Review/Review';
 import { useNavigate } from 'react-router-dom';
 
