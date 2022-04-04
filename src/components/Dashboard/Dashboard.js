e43210fc0bb884182b084a7b59983aa7a5ea89a1
najmuleimon <najmul.eimon@gmail.com>
2022-04-04 20:57:30 +0600

    dashboard charts added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,101 @@
 import React from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Dashboard.css';
 
 const Dashboard = () => {
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
     return (
-        <div>
-            <p>this is dashboard</p>
+        <div className="all-charts">
+            <div>
+                <LineChart width={600} height={400} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="sell" stroke="#ff4d1c" />
+                </LineChart>
+            </div>
+            <div>
+                <AreaChart width={600} height={400} data={data}
+                margin={{ top: 10, right: 30, left: 20, bottom: 0 }}>
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                    </linearGradient>
+                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                </AreaChart>
+            </div>
+            <div>
+            <BarChart width={600} height={400} data={data} margin={{top: 5, right: 30, left: 20, bottom: 5,}}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="investment" fill="#8884d8" />
+                <Bar dataKey="revenue" fill="#82ca9d" />
+                </BarChart>
+            </div>
+            <div>
+            <PieChart width={500} height={500}>
+                <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+            </PieChart>
+            </div>
         </div>
+        
     );
 };
 
